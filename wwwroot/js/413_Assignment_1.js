665c1cfb6fbb9778aa99bf193eab9019bf1c8a7e
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-01-20 18:06:20 -0700

    Add project files.

---
+++ wwwroot/js/413_Assignment_1.js
@@ -0,0 +1,11 @@
+﻿var submit = document.getElementById("submitbutton");
+
+submit.addEventListener("click", function () {
+    var Assignments = document.getElementById("Assignments").value;
+    var Group_Projects = document.getElementById("Group Project").value;
+    var Quizzes = document.getElementById("Quizzes").value;
+    var Exams = document.getElementById("Exams").value;
+    var Intex = document.getElementById("Intex").value;
+    var alert_text = parseInt(Assignments) * .5 + parseInt(Group_Projects) * .1 + parseInt(Quizzes) * .1 + parseInt(Exams) * .2 + parseInt(Intex) * .1;
+    alert(String(alert_text));
+})
